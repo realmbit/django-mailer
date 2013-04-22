3030490e7e69103a672fd3638e019313d949918c
Jesse <jesse.heitler@torqueim.com>
2013-04-23 00:17:23 +0100

    Undo changes related to priority send.

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -38,15 +38,11 @@ def send_mail(subject, message, from_email, recipient_list, priority="medium",
     subject = force_unicode(subject)
     message = force_unicode(message)
     
-    message = make_message(subject=subject,
-                           body=message,
-                           from_email=from_email,
-                           to=recipient_list,
-                           priority=priority)
-    if priority == "high":
-        send_now(message)
-    else:
-        message.save()
+    make_message(subject=subject,
+                 body=message,
+                 from_email=from_email,
+                 to=recipient_list,
+                 priority=priority).save()
     return 1
 
 
@@ -75,10 +71,7 @@ def send_html_mail(subject, message, message_html, from_email, recipient_list,
     email = EmailMultiAlternatives(email.subject, email.body, email.from_email, email.to)
     email.attach_alternative(message_html, "text/html")
     msg.email = email
-    if priority == "high":
-        send_now(msg)
-    else:
-        msg.save()
+    msg.save()
     return 1
 
 
@@ -109,11 +102,3 @@ def mail_managers(subject, message, fail_silently=False, connection=None, priori
                      message,
                      settings.SERVER_EMAIL,
                      [a[1] for a in settings.MANAGERS])
-
-def send_now(message):
-    connection = get_connection(backend=EMAIL_BACKEND)
-    logging.info("sending message now '%s' to %s" % (message.subject.encode("utf-8"), u", ".join(message.to_addresses).encode("utf-8")))
-    email = message.email
-    email.connection = connection
-    email.send()
-    MessageLog.objects.log(message, 1) # @@@ avoid using literal result code
